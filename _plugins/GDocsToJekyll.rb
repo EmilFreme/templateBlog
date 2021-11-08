381e595372f29ace09099905702010be35ce032f
Emil Freme <pedrohepf@insper.edu.br>
2021-11-08 16:35:07 -0300

    Parsed HTML

--- _plugins/GDocsToJekyll.rb
+++ _plugins/GDocsToJekyll.rb
@@ -1,4 +1,5 @@
 require 'http'
+require 'nokogiri'
 require 'pandoc-ruby'
 require 'cgi'
 module Jekyll
@@ -23,16 +24,45 @@ module Jekyll
 
       if res.code == 200
         resBody = res.body.dup 
-        mddoc = PandocRuby.convert("#{resBody}", from: :html, to: :markdown_mmd).force_encoding("UTF-8")
-        "#{mddoc}"
-      else
-        "FUCK #{res.body}"
-      end
+        parsedHTML = Nokogiri::HTML5(resBody)
 
-      
+        for node in parsed.css("span")
+          if(node["style"])
+            styleCheck = node["style"]
+            # TW: 900 FS:22  H1
+            if (styleCheck.include? "font-weight:900" and \
+                styleCheck.include? "font-size:22")
+              node.delete("style")
+              newNode = parsed.create_element("h1")
+              newNode.inner_html = node.inner_html
+              node.replace(newNode)
+              # TW:700 b
+            elsif (styleCheck.include? "font-weight:700")
+              node.delete("style")
+              newNode = parsed.create_element("b")
+              newNode.inner_html = node.inner_html
+              node.replace(newNode)
+              # TW: 400 FS 12  Basetext
+            elsif( styleCheck.include? "font-weight:400" and \
+                  styleCheck.include? "font-size:12")
+              node.delete("style")
+            end
+          end
+        end
+
+        for node in parsed.css("*")
+          node.delete("style")
+
+          mddoc = PandocRuby.convert("#{parsedHTML.to_html}", from: :html, to: :markdown_mmd).force_encoding("UTF-8")
+          "#{mddoc}"
+        else
+          "FUCK #{res.body}"
+        end
+
+
+      end
     end
+
   end
 
-end
-
-Liquid::Template.register_tag('gdocs2jekyll', Jekyll::GDocsToJekyll)
+  Liquid::Template.register_tag('gdocs2jekyll', Jekyll::GDocsToJekyll)
